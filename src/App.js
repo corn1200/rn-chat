71996391ce43d0a06d8b7107c9e9470acdecfac8
corn1200 <corn120033@gmail.com>
2021-12-26 02:18:08 +0900

    아이콘과 스플래쉬 이미지 변경

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { theme } from "./theme";
+import { ThemeProvider } from "styled-components";
+import Navigation from "./navigations";
+
+const App = () => {
+  console.log("all rights")
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar backgroundColor={theme.backgroundColor} barStyle="dark-content" />
+      <Navigation />
+    </ThemeProvider>
+  );
+};
+
+export default App;
