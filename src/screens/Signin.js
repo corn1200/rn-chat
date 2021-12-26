060d9b900a701722f54b75ee2a068ba5cf558b5d
corn1200 <corn120033@gmail.com>
2021-12-26 16:35:10 +0900

    로그인 화면 스크롤 기능 추가

--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -3,6 +3,7 @@ import { Button, Image, Input } from "../components";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useContext, useState, useRef } from "react";
 import { ThemeContext } from "styled-components/native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 // 로그인 페이지를 감싸는 컴포넌트
 const Container = styled.View`
@@ -33,39 +34,46 @@ const Signin = ({ navigation }) => {
         console.log('signin');
     }
     return (
-        <Container insets={insets}>
-            <Image url={LOGO} />
-            <Input
-                label="Email"
-                placeholder="Email"
-                returnKeyType="next"
-                value={email}
-                onChangeText={setEmail}
-                onSubmitEditing={() => refPassword.current.focus()}
-            />
-            <Input
-                ref={refPassword}
-                label="Password"
-                placeholder="Password"
-                returnKeyType="done"
-                value={password}
-                onChangeText={setPassword}
-                isPassword={true}
-                onSubmitEditing={_handleSigninBtnPress}
-            />
-            {/* 클릭 시 네비게이션에 Signup 으로 등록된 화면으로 이동 */}
-            <Button
-                title="Sign in"
-                onPress={_handleSigninBtnPress}
-            />
-            {/* 버튼의 스타일을 정의함 */}
-            <Button
-                title="or Sign up"
-                onPress={() => navigation.navigate('Signup')}
-                containerStyle={{ marginTop: 0, backgroundColor: 'trasparent' }}
-                textStyle={{ color: theme.btnTextLink, fontSize: 18 }}
-            />
-        </Container>
+        // 로그인 화면이 스크롤 가능하도록 만들고 인풋 포커스 시 스크롤 이동하게함
+        // 로그인 화면이 전체 화면을 이용하도록 설정
+        <KeyboardAwareScrollView
+            extraScrollHeight={20}
+            contentContainerStyle={{ flex: 1 }}
+        >
+            <Container insets={insets}>
+                <Image url={LOGO} />
+                <Input
+                    label="Email"
+                    placeholder="Email"
+                    returnKeyType="next"
+                    value={email}
+                    onChangeText={setEmail}
+                    onSubmitEditing={() => refPassword.current.focus()}
+                />
+                <Input
+                    ref={refPassword}
+                    label="Password"
+                    placeholder="Password"
+                    returnKeyType="done"
+                    value={password}
+                    onChangeText={setPassword}
+                    isPassword={true}
+                    onSubmitEditing={_handleSigninBtnPress}
+                />
+                {/* 클릭 시 네비게이션에 Signup 으로 등록된 화면으로 이동 */}
+                <Button
+                    title="Sign in"
+                    onPress={_handleSigninBtnPress}
+                />
+                {/* 버튼의 스타일을 정의함 */}
+                <Button
+                    title="or Sign up"
+                    onPress={() => navigation.navigate('Signup')}
+                    containerStyle={{ marginTop: 0, backgroundColor: 'trasparent' }}
+                    textStyle={{ color: theme.btnTextLink, fontSize: 18 }}
+                />
+            </Container>
+        </KeyboardAwareScrollView>
     );
 };
 
