e4fd6173ca27a3eb3d948e6eb75234777269a96f
corn1200 <corn120033@gmail.com>
2021-12-26 22:13:28 +0900

    파이어베이스 이메일 로그인 기능 추가

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,13 @@
-import * as firebase from 'firebase';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import config from '../firebase.json';
 
 // 파이어베이스 설정을 불러온다
-const app = firebase.initializeApp(config);
+const app = initializeApp(config);
+const auth = getAuth(app);
+
+// 이메일과 비밀번호로 로그인하고 유저 오브젝트 반환
+export const signin = async ({ email, password }) => {
+    const { user } = await signInWithEmailAndPassword(auth, email, password);
+    return user;
+};
