413b48be07fb8a4972684cc7b7846af74eda1149
corn1200 <corn120033@gmail.com>
2021-12-27 01:02:21 +0900

    네비게이션에서 로딩이나 회원 정보 컨텍스트를 기반으로 보여줄 화면 분기

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,11 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
 import Auth from "./Auth";
+import { UserContext, ProgressContext } from "../contexts";
+import { useContext } from "react";
+import Main from "./Main";
+import { Spinner } from "../components";
 
 // 네비게이션에 로그인 관련 페이지 추가
 const Navigation = () => {
+    // 회원 정보와 로딩 정보를 불러온다
+    const { user } = useContext(UserContext);
+    const { inProgress } = useContext(ProgressContext);
+
     return (
         <NavigationContainer>
-            <Auth />
+            {/* 현재 로그인 상태로 네비게이션 분기 */}
+            {user.uid ? <Main /> : <Auth />}
+            {/* 로딩이 필요한 구간일 시 스피너 동작 */}
+            {inProgress && <Spinner />}
         </NavigationContainer>
     );
 };
