21fe7bccdacbdf774638df9d77c50eadb3ec1edb
corn1200 <corn120033@gmail.com>
2021-12-26 22:17:09 +0900

    버튼 클릭 비활성화 분기 및 비활성화 스타일 추가

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,6 +11,8 @@ const Container = styled.View`
     justify-content: center;
     align-items: center;
     border-radius: 4px;
+    /* 버튼 클릭 불가 상태일 경우 반투명하게 만듦 */
+    opacity: ${({disabled}) => disabled ? 0.5 : 1};
 `;
 
 // 텍스트를 보여주는 스타일 컴포넌트 생성
@@ -20,10 +22,16 @@ const Title = styled.Text`
 `;
 
 // 터치할 수 있는 버튼 컴포넌트를 생성
-const Button = ({ title, onPress, containerStyle, textStyle }) => {
+const Button = ({ title, onPress, containerStyle, textStyle, disabled }) => {
     return (
-        <TouchableOpacity onPress={onPress} style={{ flexDirection: 'row' }}>
-            <Container style={containerStyle}>
+        // disabled props가 true일 시 터치 불가 및 비활성화 전용 스타일을 적용
+        <TouchableOpacity 
+            onPress={onPress} 
+            style={{ flexDirection: 'row' }}
+            disabled={disabled}>
+            <Container 
+                style={containerStyle} 
+                disabled={disabled}>
                 <Title style={textStyle}>{title}</Title>
             </Container>
         </TouchableOpacity>
