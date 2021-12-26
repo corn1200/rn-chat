3df21403e0b43933e2ba6ff1e8b9ecf5ddbb6843
corn1200 <corn120033@gmail.com>
2021-12-27 00:58:28 +0900

    전체 페이지에서 로딩 정보, 회원 정보를 사용할 수 있게 한다

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { StatusBar } from "react-native";
 import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
 import Navigation from "./navigations";
+import { UserProvider, ProgressProvider } from "./contexts";
 
 const App = () => {
   console.log("all rights");
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar backgroundColor={theme.backgroundColor} barStyle="dark-content" />
-      <Navigation />
+      {/* 로딩에 대한 컨텍스트를 전체 페이지에서 사용할 수 있게 함 */}
+      <ProgressProvider>
+        {/* 회원 정보에 대한 컨텍스트를 전체 페이지에서 사용할 수 있게 함 */}
+        <UserProvider>
+          <StatusBar backgroundColor={theme.backgroundColor} barStyle="dark-content" />
+          <Navigation />
+        </UserProvider>
+      </ProgressProvider>
     </ThemeProvider>
   );
 };
