30602e78cb5a70582ab6d2181ca126707d79552a
corn1200 <corn120033@gmail.com>
2021-12-26 16:28:54 +0900

    회원가입 화면 입력 기능 추가

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,26 +1,83 @@
 import styled from "styled-components/native";
+import { Button, Image, Input } from "../components";
+import { useState, useRef } from "react";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-// 화면 전체를 감싸는 컴포넌트
+// 로그인 페이지를 감싸는 컴포넌트
 const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-    background-color: ${({theme}) => theme.background};
-    padding: 0 20px;
+    background-color: ${({ theme }) => theme.background};
+    padding: 50px 20px;
 `;
 
-// 텍스트를 출력하는 스타일 컴포넌트
-const StyledText = styled.Text`
-    font-size: 30px;
-    color: #111;
-`;
-
-// 회원가입 화면 컴포넌트
+// 로그인 화면 컴포넌트
 const Signup = () => {
+    // 입력을 저장하는 state
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+    // 키보드에서 다음 버튼을 눌렀을 때 다음 입력으로 이동되게 함
+    const refEmail = useRef(null);
+    const refPassword = useRef(null);
+    const refPasswordConfirm = useRef(null);
+
+    const _handleSignupBtnPress = () => {
+        console.log('Signup');
+    }
     return (
-        <Container>
-            <StyledText>Signup</StyledText>
-        </Container>
+        // 스크롤 기능과 인풋 포커스 시 스크롤 이동되는 컴포넌트
+        <KeyboardAwareScrollView
+            extraScrollHeight={20}
+        >
+            <Container>
+                <Image />
+                <Input
+                    label="Name"
+                    placeholder="Name"
+                    returnKeyType="next"
+                    value={name}
+                    onChangeText={setName}
+                    onSubmitEditing={() => refEmail.current.focus()}
+                />
+                <Input
+                    ref={refEmail}
+                    label="Email"
+                    placeholder="Email"
+                    returnKeyType="next"
+                    value={email}
+                    onChangeText={setEmail}
+                    onSubmitEditing={() => refPassword.current.focus()}
+                />
+                <Input
+                    ref={refPassword}
+                    label="Password"
+                    placeholder="Password"
+                    returnKeyType="next"
+                    value={password}
+                    onChangeText={setPassword}
+                    isPassword={true}
+                    onSubmitEditing={() => refPasswordConfirm.current.focus()}
+                />
+                <Input
+                    ref={refPasswordConfirm}
+                    label="Password Confirm"
+                    placeholder="Password"
+                    returnKeyType="done"
+                    value={passwordConfirm}
+                    onChangeText={setPasswordConfirm}
+                    isPassword={true}
+                    onSubmitEditing={_handleSignupBtnPress}
+                />
+                {/* 클릭 시 네비게이션에 Signup 으로 등록된 화면으로 이동 */}
+                <Button
+                    title="Sign up"
+                    onPress={_handleSignupBtnPress}
+                />
+            </Container>
+        </KeyboardAwareScrollView>
     );
 };
 
